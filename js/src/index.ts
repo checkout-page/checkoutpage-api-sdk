d680e3db4d98d3565fd00bd431d00ac6766652fe
Matt-Gould29 <matt@checkoutpage.co>
2026-03-12 14:24:41 +0700

    refactor: remove old pages from api sdk

--- js/src/index.ts
+++ js/src/index.ts
@@ -5,10 +5,6 @@ import { PaymentResource } from './resources/payments/payments';
 import { SubscriptionResource } from './resources/subscriptions/subscriptions';
 import { BookingResource } from './resources/bookings/bookings';
 import { TicketResource } from './resources/tickets/tickets';
-import { PageResource } from './resources/pages/pages';
-import { PageFieldResource } from './resources/pages/fields';
-import { PageTicketGroupResource } from './resources/pages/ticket-groups';
-import { PageTicketTypeResource } from './resources/pages/ticket-types';
 import { ProductResource } from './resources/products/products';
 import { FileResource } from './resources/files/files';
 
@@ -19,10 +15,6 @@ export class CheckoutPageClient {
   public readonly subscriptions: SubscriptionResource;
   public readonly bookings: BookingResource;
   public readonly tickets: TicketResource;
-  public readonly pages: PageResource;
-  public readonly pageFields: PageFieldResource;
-  public readonly pageTicketGroups: PageTicketGroupResource;
-  public readonly pageTicketTypes: PageTicketTypeResource;
   public readonly products: ProductResource;
   public readonly files: FileResource;
   private readonly client: CheckoutPageApiClient;
@@ -35,10 +27,6 @@ export class CheckoutPageClient {
     this.subscriptions = new SubscriptionResource(this.client);
     this.bookings = new BookingResource(this.client);
     this.tickets = new TicketResource(this.client);
-    this.pages = new PageResource(this.client);
-    this.pageFields = new PageFieldResource(this.client);
-    this.pageTicketGroups = new PageTicketGroupResource(this.client);
-    this.pageTicketTypes = new PageTicketTypeResource(this.client);
     this.products = new ProductResource(this.client);
     this.files = new FileResource(this.client);
   }
@@ -62,19 +50,6 @@ export type {
   SubscriptionList,
   ValidateTicketResponse,
   ValidateTicketParams,
-  Page,
-  PageList,
-  PageListParams,
-  CreatePageParams,
-  UpdatePageParams,
-  PageField,
-  PageFieldList,
-  CreatePageFieldParams,
-  UpdatePageFieldParams,
-  CreateTicketGroupParams,
-  UpdateTicketGroupParams,
-  CreateTicketTypeParams,
-  UpdateTicketTypeParams,
   Product,
   UpdateProductParams,
   UploadFileResponse,
