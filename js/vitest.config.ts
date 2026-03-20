3d2fc4eabf0c06c8915200696d8da9aac736181c
Matt-Gould29 <matt@checkoutpage.co>
2026-03-20 08:39:23 +0700

    feat: increase test timeout

--- js/vitest.config.ts
+++ js/vitest.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
+    testTimeout: 20000,
     include: ['**/*.{test,spec}.{ts,tsx}', '**/*.integration.test.ts'],
     coverage: {
       provider: 'v8',
